d04653cddd85c6bdc81c2ff34c303fa625be5f61
Soma Sekine <a8121151@aoyama.jp>
2024-06-07 23:12:06 +0900

    GitHubでのログイン成功

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,19 +1,33 @@
-import UserInfo from "@/components/UserInfo";
-import { getServerAuthSession } from "@/server/auth";
-import Link from "next/link";
+"use client"
 
-export default async function HomePage() {
-  const authSession = await getServerAuthSession(); //(1) 
+import { useSession, signIn, signOut } from "next-auth/react";
+import Image from "next/image";
 
-  return (  
-  <main className="flex items-center justify-center h-screen">
-    {authSession ? "authenticated" : "not authenticated"}
-    {authSession?.user && <UserInfo user={authSession?.user} />}
-    {!authSession?.user && ( //(3) 
-      <Link className="font-medium mt-2 text-blue-600 hover:underline" href="/login">
-        Login
-      </Link>
-    )}
-  </main>
-  );
+export default function Home() {
+  const { data: session } = useSession();
+
+  if(session) {
+    return (
+      <div className="w-full h-screen flex flex-col justify-center items-center">
+        <div className="w-44 h-44 relative mb-4">
+        <Image
+          src={session.user?.image as string}
+          fill
+          alt=""
+          className="object-cover rounded-full"
+        />
+        </div>
+        <p className="text-2xl mb-2">Welcome <span className="font-bold">{session.user?.name}</span>. Signed In As</p>
+        <p className="font-bold mb-4">{session.user?.email}</p>
+        <button className="bg-red-600 py-2 px-6 rounded-md" onClick={() => signOut()}>Sign out</button>
+      </div>
+    )
+  }
+
+  return (
+    <div className="w-full h-screen flex flex-col justify-center items-center">
+        <p className="text-2xl mb-2">Not Signed In</p>
+        <button className="bg-none border-gray-300 border py-2 px-6 rounded-md mb-2" onClick={() => signIn('github')}>Sign in with github</button>
+    </div>
+  )
 }
