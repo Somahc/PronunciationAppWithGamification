7c4aad474ab4cdacb6d1f5a29d238dd46c8c4cd1
Soma Sekine <a8121151@aoyama.jp>
2024-06-11 22:38:27 +0900

    DB連携完了

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,8 +1,12 @@
 import NextAuth from 'next-auth';
 import GithubProvider from "next-auth/providers/github"
 import GoogleProvider from "next-auth/providers/google"
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import prisma from "../../../lib/prisma";
+import type { Adapter } from 'next-auth/adapters';
 
 const handler = NextAuth({
+    adapter: PrismaAdapter(prisma) as Adapter,
     providers: [
         GithubProvider({
             clientId: process.env.GITHUB_ID as string,
@@ -12,7 +16,12 @@ const handler = NextAuth({
             clientId: process.env.GOOGLE_ID as string,
             clientSecret: process.env.GOOGLE_SECRET as string,
         }),
-    ]
+    ],
+    session: {
+        strategy: "database",
+        maxAge: 60 * 60 * 24 * 30, // 30 days
+        updateAge: 60 * 60 * 24, // 24 hours
+      },
 });
 
 export { handler as GET, handler as POST }
