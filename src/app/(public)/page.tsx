9522727bbdb2f50a91d9010d3721a40fc4bf61c4
Soma Sekine <a8121151@aoyama.jp>
2024-09-23 19:11:19 +0900

    ログイン時の注意追加

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -8,8 +8,14 @@ export default async function Page() {
 
   if (!session) {
     return (
-      <div className="w-full h-screen flex justify-center items-center">
-        <div className="text-sm">右上の「ログイン」ボタンからログインしてください</div>
+      <div className="w-full h-screen flex flex-col justify-center items-center">
+        <div className="p-4">
+          <p className="text-sm">右上の「ログイン」ボタンからログインしてください</p>
+          <br />
+          <p className="text-sm text-red-500">注意：ログインにはGoogleアカウントを利用し、アカウントに登録された名前とプロフィール画像が本Webアプリ内で他のユーザーへ公開されます。</p>
+          <br />
+          <p className="text-sm">Googleアカウントを持っていない、あるいは既存のGoogleアカウントを使いたくない場合は、適当な名前をつけたGoogleアカウントを作成し、ログインしてください。</p>
+        </div>
       </div>
     )
   }
