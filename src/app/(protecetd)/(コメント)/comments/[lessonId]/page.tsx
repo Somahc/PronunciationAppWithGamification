0de14812214ac2c3c575142a80541695c55e4860
Soma Sekine <a8121151@aoyama.jp>
2024-09-27 19:35:50 +0900

    コメント欄スタイル修正

--- src/app/(protecetd)/(コメント)/comments/[lessonId]/page.tsx
+++ src/app/(protecetd)/(コメント)/comments/[lessonId]/page.tsx
@@ -20,12 +20,12 @@ export default async function Page({ params }: { params: { lessonId: string } })
         <div className="relative w-full min-h-screen flex flex-col items-center">
             <div>コメント一覧</div>
             {comments.map((comment: Comment, index) => (
-                <div className="mt-5 border border-gray-300 p-4 rounded-md" key={index}>
-                    <div className="flex">
-                        <div className="mr-3 font-bold">{comment.user ? comment.user.name : 'Unknownさん'}</div>
-                        <time>{format(comment.createdAt, 'yyyy/MM/dd')}</time>
+                <div className="mt-5 w-[80%] bg-white p-4 rounded-md" key={index}>
+                    <div className="flex items-center">
+                        <div className="mr-3 font-extrabold">{comment.user ? comment.user.name : 'Unknownさん'}</div>
+                        <time className="text-xs">{format(comment.createdAt, 'yyyy/MM/dd')}</time>
                     </div>
-                    <p>{comment.content}</p>
+                    <p className="p-1">{comment.content}</p>
                 </div>
             ))}
             <PostCommentModal lessonId={lessonId} />
