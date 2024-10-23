3502be0ea42e080cbf12bb67ce99006f79d38c6a
Soma Sekine <a8121151@aoyama.jp>
2024-10-24 01:42:38 +0900

    注意書き更新（なし）

--- src/app/(public)/page.tsx
+++ src/app/(public)/page.tsx
@@ -1,7 +1,7 @@
 import { getServerSession } from "next-auth";
 import { nextAuthOptions } from "@/app/lib/next-auth/options";
+import Image from "next/image";
 import Title from "@/app/components/Title";
-import TitleNashi from "../components/TitleNashi";
 
 export default async function Page() {
 
@@ -13,7 +13,11 @@ export default async function Page() {
         <div className="p-4">
           <p className="text-sm">右上の「ログイン」ボタンからログインしてください</p>
           <br />
-          <p className="text-sm text-red-500">注意：ログインにはGoogleアカウントを利用し、アカウントに登録された名前とプロフィール画像が本Webアプリ内で他のユーザーへ公開されます。</p>
+          <p className="text-sm text-red-500">注意：ログインにはGoogleアカウントを利用し、アカウントに登録された名前が本Webアプリ内でのみ他ユーザーに公開されます。</p>
+          <small>※名前とは下図の青丸で囲っている部分で表示されるものです。</small>
+          <div>
+          <Image src="/assets/google_name.png" width={1076} height={585} alt="s"/>
+          </div>
           <br />
           <p className="text-sm">Googleアカウントを持っていない、あるいは既存のGoogleアカウントを使いたくない場合は、適当な名前をつけたGoogleアカウントを作成し、ログインしてください。</p>
         </div>
@@ -23,8 +27,7 @@ export default async function Page() {
 
   return (
     <>
-      {/* <Title /> */}
-      <TitleNashi />
+      <Title />
     </>
   )
 }
